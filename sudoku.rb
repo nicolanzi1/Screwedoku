639cd2edf8545d15a82baaf43e4608a74e8e4289
nicolanzi1 <nicolanzi@gmail.com>
2020-06-20 15:43:29 +0100

    Created method Rows in board.rb, parse_pos and parse_val in suoku.rb, added command game.run and put board to define initialize

--- sudoku.rb
+++ sudoku.rb
@@ -10,33 +10,24 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
-  end
+  # def method_missing(method_name, *args)
+  #   if method_name =~ /val/
+  #     Integer(1)
+  #   else
+  #     string = args[0]
+  #     string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+  #   end
+  # end
 
   def get_pos
     pos = nil
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
-
-      begin
-        pos = parse_pos(gets.chomp)
-      rescue
-        # TODO: Google how to print the error that happened inside of a rescue statement.
-        puts "Invalid position entered (did you use a comma?)"
-        puts ""
-
-        pos = nil
-      end
+      pos = parse_pos(gets.chomp)
     end
     pos
   end
@@ -49,6 +40,14 @@ class SudokuGame
       val = parse_val(gets.chomp)
     end
     val
+  end
+
+  def parse_pos(string)
+    string.split(",").map! { |char| Integer(char) }
+  end
+
+  def parse_val(string)
+    Integer(string)
   end
 
   def play_turn
@@ -85,3 +84,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
